801fad7f30411ba3d47dea32da8be06aaa196ba4
Mitch1092 <mitchbart666@hotmail.com>
2025-04-30 16:47:08 -0600

    Primer commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void mostrarTareas(){
+
+}
+
+void eliminarTarea(){
+
+}
+
+void agregarTarea(){
+
+}
+
+void guardarEnArchivo(){
+
+}
+
+void cargarDesdeArchivo(){
+
+}
+
+void menu(){
+
+}
+
+int main() {
+
+}
