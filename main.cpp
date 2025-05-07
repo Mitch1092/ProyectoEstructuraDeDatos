1958d6c8a0104140436cc02c6ebd2033815f8a57
Mitch1092 <mitchbart666@hotmail.com>
2025-05-07 16:54:35 -0600

    Done.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
+#include <fstream>
 #include <iostream>
 #include "json.hpp"
 #include "Lista.cpp"
 using namespace std;
 using json = nlohmann::json;
 
-
+int leerInt() {
+    int n;
+    cin >> n;
+    while (cin.fail() || n < 0){
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        cout << "Valor invalido. Intente de nuevo: " ;
+        cin >> n;
+    }
+    return n;
+}
 void mostrarTareas(Nodo a){
     a.mostrar();
 }
@@ -31,7 +42,8 @@ Nodo agregarTarea(Nodo a){
     int prioridad;
     string nombre, descripcion;
     cout << "Nombre de la tarea: ";
-    cin >> nombre;
+    cin.ignore();
+    getline(cin, nombre, '\n');
     cout << "Prioridad: ";
     cin >> prioridad;
     while (cin.fail() || prioridad < 0){
@@ -41,7 +53,8 @@ Nodo agregarTarea(Nodo a){
         cin >> prioridad;
     }
     cout << "Descripcion: ";
-    cin >> descripcion;
+    cin.ignore();
+    getline(cin, descripcion, '\n');
     a.insertar(prioridad,nombre,descripcion);
     return a;
 }
@@ -72,12 +85,51 @@ Nodo editarTarea(Nodo a) {
     return a;
 }
 
-void guardarEnArchivo(){
+void guardarEnArchivo(Nodo a){
+    ofstream file("Data.json");
 
+    if (a.empty()) {
+        file.close();
+        cout << "La lista esta vacia, no hat nada para exportar" << endl;
+        return;
+    }
+
+    json tareas = json::array();
+    Nodo *aux = a.cabeza;
+    while (aux != NULL) {
+        json tarea = {
+            {"prioridad",aux -> prioridad},
+            {"nombre", aux -> nombre},
+            {"descripcion", aux -> descripcion},
+        };
+        tareas.push_back(tarea);
+        aux = aux -> sig;
+    }
+    file << tareas.dump(3) << endl;
+    file.close();
+    cout << "Se exportaron las tareas";
 }
 
-void cargarDesdeArchivo(){
+Nodo cargarDesdeArchivo(Nodo a){
+    ifstream file;
+    file.open("Data.json");
+    json tareas;
+    file >> tareas;
+    int b = tareas.size();
+    file.close();
+    if (b == 0) {
+        cout << "No hay nada en memoria" << endl;
+        return a;
+    }
 
+    for (const auto& i : tareas) {
+        int prioridad = i["prioridad"].get<int>();
+        string nombre = i["nombre"].get<string>();
+        string descripcion = i["descripcion"].get<string>();
+        a.insertar(prioridad,nombre,descripcion);
+    }
+    cout << "Se importaron las tareas." << endl;
+    return a;
 }
 
 
@@ -114,10 +166,10 @@ void menu(Nodo nodo){
                 nodo = eliminarTarea(nodo);
                 break;
             case 5:
-                cargarDesdeArchivo();
+                nodo = cargarDesdeArchivo(nodo);
                 break;
             case 6:
-                guardarEnArchivo();
+                guardarEnArchivo(nodo);
                 break;
             case 7:
                 cout << "Cerrando el sistema." << endl;
